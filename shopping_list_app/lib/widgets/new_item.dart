ddd333b0777ac548b527e24e8bec56dfe2ea0b28
Matvii <matveyulitskiy@gmail.com>
2025-03-27 22:34:04 +0100

    Last version

--- shopping_list_app/lib/widgets/new_item.dart
+++ shopping_list_app/lib/widgets/new_item.dart
@@ -1,21 +1,60 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/categories.dart';
+import 'package:shopping_list_app/models/category.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
 
   @override
-  State<NewItem> createState() => _NewItemState();
+  State<NewItem> createState() {
+    return _NewItemState();
+  }
 }
 
 class _NewItemState extends State<NewItem> {
   final _formKey = GlobalKey<FormState>();
   var enteredName = '';
   var enteredQuantity = 1;
+  var selectedCategory = categories[Categories.vegetables]!;
+  var isSending = false;
 
-  void saveItem() {
-    if(_formKey.currentState!.validate()){
+  void _saveItem() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        isSending = true;
+      });
       _formKey.currentState!.save();
+      final url = Uri.https(
+        'shopping-list-app-562e2-default-rtdb.firebaseio.com',
+        'shopping-list.json',
+      );
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          'name': enteredName,
+          'quantity': enteredQuantity,
+          'category': selectedCategory.title,
+        }),
+      );
+
+      final Map<String, dynamic> resData = json.decode(response.body);
+
+      if (!context.mounted) {
+        return;
+      }
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: resData['name'],
+          name: enteredName,
+          quantity: enteredQuantity,
+          category: selectedCategory,
+        ),
+      );
     }
   }
 
@@ -24,20 +63,20 @@ class _NewItemState extends State<NewItem> {
     return Scaffold(
       appBar: AppBar(title: const Text('Add a new item')),
       body: Padding(
-        padding: EdgeInsets.all(12),
+        padding: const EdgeInsets.all(12),
         child: Form(
           key: _formKey,
           child: Column(
             children: [
               TextFormField(
                 maxLength: 50,
-                decoration: InputDecoration(label: Text('Name')),
+                decoration: const InputDecoration(label: Text('Name')),
                 validator: (value) {
                   if (value == null ||
                       value.isEmpty ||
-                      int.tryParse(value) == null ||
-                      int.tryParse(value)! <= 0) {
-                    return 'Must be a valid, positive number';
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characters.';
                   }
                   return null;
                 },
@@ -50,14 +89,29 @@ class _NewItemState extends State<NewItem> {
                 children: [
                   Expanded(
                     child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
                       keyboardType: TextInputType.number,
-                      decoration: InputDecoration(label: Text('Quantity')),
                       initialValue: enteredQuantity.toString(),
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a valid, positive number.';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        enteredQuantity = int.parse(value!);
+                      },
                     ),
                   ),
-                  SizedBox(width: 8),
+                  const SizedBox(width: 8),
                   Expanded(
                     child: DropdownButtonFormField(
+                      value: selectedCategory,
                       items: [
                         for (final category in categories.entries)
                           DropdownMenuItem(
@@ -69,28 +123,45 @@ class _NewItemState extends State<NewItem> {
                                   height: 16,
                                   color: category.value.color,
                                 ),
-                                SizedBox(width: 6),
+                                const SizedBox(width: 6),
                                 Text(category.value.title),
                               ],
                             ),
                           ),
                       ],
-                      onChanged: (value) {},
+                      onChanged: (value) {
+                        setState(() {
+                          selectedCategory = value!;
+                        });
+                      },
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 12),
+              const SizedBox(height: 12),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      _formKey.currentState!.reset();
-                    },
-                    child: Text('Reset'),
+                    onPressed:
+                        isSending
+                            ? null
+                            : () {
+                              _formKey.currentState!.reset();
+                            },
+                    child: const Text('Reset'),
                   ),
-                  ElevatedButton(onPressed: saveItem, child: Text('Add Item')),
+                  ElevatedButton(
+                    onPressed: isSending ? null : _saveItem,
+                    child:
+                        isSending
+                            ? const SizedBox(
+                              height: 16,
+                              width: 16,
+                              child: CircularProgressIndicator(),
+                            )
+                            : const Text('Add Item'),
+                  ),
                 ],
               ),
             ],
